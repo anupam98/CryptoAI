a69130a810b1d22653adba98adc604cb394b9c9e
Anupam Adhikari <anupam.adhikari13@gmail.com>
2025-07-23 16:21:32 -0500

    changing some import statements

--- src/shared/agents/recommendation_agent.py
+++ src/shared/agents/recommendation_agent.py
@@ -1,6 +1,6 @@
 from crewai import Agent, LLM
 from dotenv import load_dotenv
-from tools.custom_tool import DocumentSearchTool
+from shared.tools.custom_tool import DocumentSearchTool  
 
 load_dotenv()
 
--- src/web_application_server/app.py
+++ src/web_application_server/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-from main import main, get_user_portfolios
+from cli_server.main import main, get_user_portfolios
 
 st.set_page_config(page_title="Crypto Portfolio Advisor", page_icon="📊")
 st.title("📊 Crypto Portfolio Advisor")
